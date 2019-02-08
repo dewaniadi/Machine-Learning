63b64c076b4dd0a5e0adb02d75fbd35b557b0383
[dewaniadi] <[adadityadewani@gmail.com]>
2019-02-08 15:35:34 +0530

    final data pre process

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -30,3 +30,10 @@ labelencoder_y = LabelEncoder()
 y=labelencoder_y.fit_transform(y)
 
 
+#splitting the data
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
+
+
+
